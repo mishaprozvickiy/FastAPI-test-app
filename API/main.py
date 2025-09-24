d7f4d5f56e1f1fecc32a1296703002fa25305658
mishaprozvickiy <mishaprozvickiy@gmail.com>
2025-09-24 14:49:46 +0700

    add port 80

--- API/main.py
+++ API/main.py
@@ -19,4 +19,4 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="0.0.0.0")
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=80)
